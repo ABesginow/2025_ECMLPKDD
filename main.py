3922d7a7f2f2099b3b067d09f17c69d9a208f42c
Andreas Besginow <andreas.besginow@th-owl.de>
2025-05-12 14:42:51 +0200

    testing new LODEGP class

---
+++ main.py
@@ -0,0 +1,9 @@
+import gpytorch
+from LODEGP.LODEGP import LODEGP
+import torch
+
+
+train_x = torch.linspace(0, 1, 100)
+train_y = torch.linspace(0, 1, 100)
+likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=3)
+LODEGP(train_x, train_y, likelihood, 3, ODE_name="Bipendulum", verbose=True)
