cf7445ade41c9b540df4b8842e1044d45f53e196
Andreas Besginow <andreas.besginow@th-owl.de>
2025-05-13 17:33:57 +0200

    Main loop now contains training and plotting. Untested state

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import gpytorch
-from LODEGP.LODEGP import LODEGP, list_standard_models
+from LODEGP.LODEGP import LODEGP
+from helpers.training_functions import granso_optimization
+from helpers.plotting_functions import plot_single_input_gp_posterior
 from sage.all import *
 import sage
 import torch
@@ -17,8 +19,7 @@ y2_func = lambda x: float(688061/800000)*torch.sin(x)/x - float(2543/4000)*torch
 y0 = y0_func(train_x)
 y1 = y1_func(train_x)
 y2 = y2_func(train_x)
-train_y = torch.stack([y0, y1, y2], dim=1)
-
+train_y = torch.stack([y0, y1, y2], dim=-1)
 
 # Bipendulum versions to test:
 # "Bipendulum", "Bipendulum first equation", "Bipendulum second equation", "Bipendulum Parameterized", "No system"
@@ -26,23 +27,46 @@ for system_name in ["Bipendulum", "Bipendulum first equation", "Bipendulum secon
     print(f"Testing system: {system_name}")
     # 1. Model definition
     likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=3)
-    LODEGP(train_x, train_y, likelihood, 3, ODE_name=system_name, verbose=True, system_parameters={"l1": 1.0, "l2": 2.0})
+    model = LODEGP(train_x, train_y, likelihood, 3, ODE_name=system_name, verbose=True, system_parameters={"l1": 1.0, "l2": 2.0})
+
+    likelihood_MAP = gpytorch.likelihoods.MultitaskGaussianLikelihood(num_tasks=3)
+    model_MAP = LODEGP(train_x, train_y, likelihood_MAP, 3, ODE_name=system_name, verbose=True, system_parameters={"l1": 1.0, "l2": 2.0})
 
 
     # 2. Model training (10 random restarts with PyGRANSO) (try MLL and MAP with uninformed prior)
+    model.train()
+    likelihood.train()
+    unscaled_neg_MLL, model, likelihood, training_logs = granso_optimization(model, likelihood, train_x, train_y, random_restarts=10, uninformed=True, logarithmic_reinit=True, verbose=True, MAP=False)
 
+    model_MAP.train()
+    likelihood_MAP.train()
+    unscaled_neg_MLL, model_MAP, likelihood_MAP, training_logs = granso_optimization(model_MAP, likelihood_MAP, train_x, train_y, random_restarts=10, uninformed=True, logarithmic_reinit=True, verbose=True, MAP=True)
 
     # 3. Draw model posterior
+    test_x = torch.linspace(START, END, COUNT)
+    plot_colors = ['red', 'green', 'blue']
+    model.eval()
+    likelihood.eval()
+    with torch.no_grad(), gpytorch.settings.fast_pred_var():
+        observed_pred = likelihood(model(test_x))
+        pred_mean = observed_pred.mean
+        pred_var = observed_pred.covariance_matrix
+        pred_var = torch.diagonal(pred_var)
+    
+    MLL_model_posterior_fig = plot_single_input_gp_posterior(train_x, train_y, test_x, pred_mean, pred_var, n_std=2, show=False, return_fig=True, fig=None, ax=None, colors=plot_colors, ncols=3, figsize=(20, 5), titles=["$f_1$", "$f_2$", "$u$"], xlabel="Time", ylabel="Output")
 
+    model_MAP.eval()
+    likelihood_MAP.eval()
+    with torch.no_grad(), gpytorch.settings.fast_pred_var():
+        observed_pred = likelihood_MAP(model_MAP(test_x))
+        pred_mean = observed_pred.mean
+        pred_var = observed_pred.covariance_matrix
+        pred_var = torch.diagonal(pred_var)
+    
+    MAP_model_posterior_fig = plot_single_input_gp_posterior(train_x, train_y, test_x, pred_mean, pred_var, n_std=2, show=False, return_fig=True, fig=None, ax=None, colors=plot_colors, ncols=3, figsize=(20, 5), titles=["$f_1$", "$f_2$", "$u$"], xlabel="Time", ylabel="Output")
+
+    # Store the figures
+    MLL_model_posterior_fig.savefig(f"MLL_model_posterior_{system_name}.png")
+    MAP_model_posterior_fig.savefig(f"MAP_model_posterior_{system_name}.png")
 
     # 4. Calculate precision? i.e. finite difference of samples on eval points
-
-
-    
-
-
-
-
-
-
-
